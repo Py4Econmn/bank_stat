35fae8667be9017d6a22a304013785fb7d332e2c
Py4Econmn <py4econ@gmail.com>
2024-02-18 00:07:58 +0100

    plot

--- banks.py
+++ banks.py
@@ -19,14 +19,25 @@ df = df[['Date','ca_mnt','ca_fx','mnt_usd']]
 df.columns= ['Date','ca_mnt','no','ca_fx','no','mnt_usd']
 df = df[['Date','ca_mnt','ca_fx','mnt_usd']]
 df['ca_fx_usd'] = df['ca_fx']/df['mnt_usd']
+# df['ca_mnt_to_fx'] = df['ca_mnt']/df['ca_fx']
+df['mnt_usd_000'] = df['mnt_usd']/1000
+df['ca_mnt_000'] = df['ca_mnt']/1e6
+df['ca_fx_usd_000'] = df['ca_fx_usd']/1e3*3
+df['ca_fx_000'] = df['ca_fx']/1e6
 
-df['d_ca_fx'] =  df['ca_fx_usd'].diff(2)/df['ca_fx_usd'].shift(2)
-df['d_mntusd'] =  df['mnt_usd'].diff().shift()/df['mnt_usd'].shift(2)
-# df['d1_ca_fx'] = df['d_ca_fx'].shift()
-
-df.plot.scatter(x='d_mntusd', y='d_ca_fx', title='CA in FX to MNT changes')
-
+df.plot(x='Date', y=['ca_mnt_000', 'ca_fx_000', 'mnt_usd_000','ca_fx_usd_000'], marker='o')
 plt.show()
+
+df.to_csv('hi.csv')
+
+
+# df['d_ca_fx'] =  df['ca_fx_usd'].diff(2)/df['ca_fx_usd'].shift(2)
+# df['d_mntusd'] =  df['mnt_usd'].diff().shift()/df['mnt_usd'].shift(2)
+# # df['d1_ca_fx'] = df['d_ca_fx'].shift()
+
+# df.plot.scatter(x='d_mntusd', y='d_ca_fx', title='CA in FX to MNT changes')
+# plt.show()
+
 
 
 
